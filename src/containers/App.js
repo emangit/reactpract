908909eec43e3a000fa4582295f8d15c4afeba8d
Evan Hardy <evanhardy@MacBook-Pro-1506.local>
2019-09-22 20:01:08 -0400

    broke up project into components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 
@@ -50,38 +51,23 @@ class App extends Component {
     let btnClass = '';
 
     if(this.state.showPersons){
-        persons = (
-            <div>
-                {this.state.persons.map((person, index) => {
-                    return <Person click={() => this.deletePersonHandler(index)}
-                                   name={person.name}
-                                   age={person.age}
-                                   key={person.id}
-                                   changed={(event) => this.nameChangedHandler(event, person.id)} />
-                })}
-
-            </div>
-        );
-
-        btnClass = classes.Red;
+        persons =
+              <Persons
+                persons={this.state.persons}
+                clicked={this.deletePersonHandler}
+                changed={this.nameChangedHandler}/>
 
     }
 
-    const assignedClasses = [];
-    if(this.state.persons.length <= 2){
-      assignedClasses.push(classes.red); //classes will be ['red']
-    }
-    if(this.state.persons.length <= 1){
-      assignedClasses.push(classes.bold); //classes will be ['red', 'bold']
-    }
+
 
     return (
 
         <div className={classes.App}>
-          <h1>I am a React app!</h1>
-          <p className={assignedClasses.join(' ')}>This is really working!</p>
-          <button className={btnClass}
-              onClick={this.togglePersonsHandler}>Toggle Persons</button>
+            <Cockpit
+              showPersons={this.state.showPersons}
+              persons={this.state.persons}
+              clicked={this.togglePersonsHandler}/>
             {persons}
         </div>
 
