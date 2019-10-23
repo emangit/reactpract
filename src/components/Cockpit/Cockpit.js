0b18f5c2c6b7c23a6f33b00b01c5551a3f57f6c9
Evan Hardy <evanhardy@MacBook-Pro-1506.local>
2019-10-23 18:13:04 -0400

    first commit

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -17,7 +17,7 @@ const cockpit = (props) => {
 
     return (
         <div className={classes.Cockpit}>
-            <h1>I am a React app!</h1>
+            <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       {id: 'asdf', name: 'Max', age: 28},
@@ -12,6 +17,15 @@ class App extends Component {
       {id: 'zxcv', name: 'Alexus', age: 26}
     ],
       showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   deletePersonHandler = (personIndex) =>{
@@ -47,8 +61,8 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
-    let btnClass = '';
 
     if(this.state.showPersons){
         persons =
@@ -65,6 +79,7 @@ class App extends Component {
 
         <div className={classes.App}>
             <Cockpit
+              title={this.props.appTitle}
               showPersons={this.state.showPersons}
               persons={this.state.persons}
               clicked={this.togglePersonsHandler}/>
